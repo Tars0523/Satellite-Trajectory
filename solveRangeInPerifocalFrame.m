e5f94c3d3fd2d3726fe1a219fc2fb9a52aaf0839
TARS <100289642+Tars0523@users.noreply.github.com>
2023-06-21 18:49:24 +0900

    Delete solveRangeInPerifocalFrame.m

--- solveRangeInPerifocalFrame.m
+++
@@ -1,6 +0,0 @@
-function rangeInPQW=solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-    true_anomaly = deg2rad(true_anomaly);
-    p = semimajor_axis*(1-eccentricity^2);
-    r = p/(1+eccentricity*cos(true_anomaly));
-    rangeInPQW = [r*cos(true_anomaly);r*sin(true_anomaly);0];
-end
