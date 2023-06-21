d52ff3c02a26130b81d7bb5ff0bee1220e2d77b0
TARS <100289642+Tars0523@users.noreply.github.com>
2023-06-21 18:49:34 +0900

    Delete solveVelocityInPerifocalFrame.m

--- solveVelocityInPerifocalFrame.m
+++
@@ -1,7 +0,0 @@
-function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-    mu = 3.986004418*10^5;
-    true_anomaly = deg2rad(true_anomaly);
-    
-    p=semimajor_axis*(1-eccentricity^2);
-    velocityInPQW = sqrt(2*mu/p)*[-sin(true_anomaly) eccentricity+cos(true_anomaly)]';
-end
